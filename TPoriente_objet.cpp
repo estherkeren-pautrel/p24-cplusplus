1e9a631cbe3bd0e364795986a2127349acbde7d8
Esther Pautrel <esther.pautrel@etu.minesparis.psl.eu>
2025-01-14 12:28:07 +0100

    erreurs de synthaxe corrigées MAIS pbm avec classes héritées

--- TPoriente_objet.cpp
+++ TPoriente_objet.cpp
@@ -3,13 +3,14 @@
 // deux méthodes : move qui passe de x, y à x + dx, y + dy ET calcul de la surface
 
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 #include <cstdlib>
 
-float pi = 3.14
+float pi = 3.14;
 
-    class forme
-{
+class forme
+{private:
     int x;
     int y;
     std::string color;
@@ -19,15 +20,13 @@ public:
     {
         x = x + dx;
         y = y + dy;
-        return 0;
     };
     bool is_at_position(int xi, int yi) {
-        return ((xi == x) and (yi == y))
-
+        return ((xi == x) and (yi == y));
     };
 };
 
-class circle : public forme()
+class circle : public forme 
 {
     int radius;
 
@@ -36,9 +35,9 @@ public:
     {
         return (pi * radius * radius);
     };
-}
+};
 
-class square : public forme()
+class square : public forme 
 {
     int side;
 
@@ -47,9 +46,9 @@ public:
     {
         return (side * side);
     };
-}
+};
 
-class rectangle : public forme()
+class rectangle : public forme
 {
     int width;
     int height;
@@ -58,30 +57,30 @@ public:
     int area()
     {
         return (width * height);
-    }
-}
+    };
+};
 
-*forme find_at_position(int xi, int yi, std::vector<*forme> v)
+forme* find_at_position(int xi, int yi, std::vector<forme*> v)
 {
     for (auto &e : v)
     {
-        if (is_at_position(xi, yi))
+        if (e->is_at_position(xi, yi))
         {
             return e;
         };
     };
     return 0;
-}
+};
 
 int main()
 {
     circle c{3, 4, 'b', 5};
     rectangle r{2, 7, 'r', 4, 7};
     std::vector<forme *> v;
-    v.pushback(&c);
-    v.pushback(&r);
+    v.push_back(&c);
+    v.push_back(&r);
     forme *s = find_at_position(2, 3, v);
     s->area();
     std::cout << s;
     return 0;
-}
+};
