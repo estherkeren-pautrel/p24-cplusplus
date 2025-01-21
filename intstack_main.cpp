8e9fcb651a93532ba0b9f20341336f3773f94250
Esther Pautrel <esther.pautrel@etu.minesparis.psl.eu>
2025-01-21 15:52:37 +0100

    avec le namespace

--- intstack_main.cpp
+++ intstack_main.cpp
@@ -1,21 +1,24 @@
 #include "intstack.h"
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
 int main()
 {
     int size;
     std::cout << "Entrez la taille de votre pile" << std::endl;
     std::cin >> size; // cin entrée standard
-    IntStack s1(size);
+    Pautrel::IntStack s1(size);
     s1.print(); // affichera [[
     s1.push(1);
     s1.push(2);
     s1.push(3);
-    IntStack j(s1);
+    Pautrel::IntStack j(s1);
     s1.print();                         // affichera [1 2 3 ]
     std::cout << s1.pop() << std::endl; // affichera 3
     s1.print();                         // affichera [1 2 ]
     j.push(4);
-    IntStack r = j;
+    Pautrel::IntStack r = j;
     r.print();
     j.print();
     return 0;
